99a3cef3aabecc68e98a94e2746133e1994d344a
H2OOPS <27hdmr@gmail.com>
2016-10-20 14:34:43 -0500

    Delete Files And Models

--- app/shared/utils/API.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var API = require("../lib/client");
-
-module.exports = new API({ 
-	key: "ade9eaa0af5b79da030f81188f3747a66a2680450548c61135487740b909e4ee",
-	secret: "Bl+yZB7Gqtumpgf1y+VcWhXGIlow8ldkd1rUZCr+62ZnlGptiqs+wNeiPULy4HkF/924hN0yqwDvT9O8w1jJYg=="
-});
